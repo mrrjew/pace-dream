5f877e9ceaf3b09b9d2fab8657c0da6a5d66230a
developer-shajib <sunhailshajib1@gmail.com>
2023-12-16 12:57:50 +0600

    Created booked user list section

--- ChisfisNextjs/src/app/layout.tsx
+++ ChisfisNextjs/src/app/layout.tsx
@@ -1,13 +1,15 @@
 import { Poppins } from "next/font/google";
 import SiteHeader from "./(client-components)/(Header)/SiteHeader";
 import ClientCommons from "./ClientCommons";
-import "./globals.css";
 import "@/fonts/line-awesome-1.3.0/css/line-awesome.css";
 import "@/styles/index.scss";
 import "rc-slider/assets/index.css";
 import Footer from "@/components/Footer";
 import FooterNav from "@/components/FooterNav";
 import ReactQueryClientProvider from "@/components/ReactQueryClientProvider";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
