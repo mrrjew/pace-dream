bf9b7b4ef502df2fc710571ddd52f7ca1967a94a
Ritik Kumar <ritik.kumar006@gmail.com>
2023-12-21 23:44:02 +0530

    comment middleware for now

--- ChisfisNextjs/src/middleware.ts
+++ ChisfisNextjs/src/middleware.ts
@@ -12,39 +12,42 @@ export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/_next')) {
     return NextResponse.next();
   }
-  const cookies = request.cookies;
-  const authToken = cookies.get('auth-token')?.value;
+  return NextResponse.next();
 
-  let isValidToken = false;
+  //TODO: uncomment this code when it is known which pages to block
+  // const cookies = request.cookies;
+  // const authToken = cookies.get('auth-token')?.value;
 
-  const checkToken = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/verify-token`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${authToken}`,
-      },
-    }
-  );
-  if (checkToken.status === 200) {
-    isValidToken = true;
-  }
-  // if the request url contains /auth/login or /auth/signin and isValidToken is true then redirect to home page
-  if (isLoginRoutes(request.nextUrl.pathname) && isValidToken) {
-    return NextResponse.redirect(new URL('/', request.nextUrl).href);
-  }
-  // if the request url contains /auth/login or /auth/signin and isValidToken is false then return the request
-  if (isLoginRoutes(request.nextUrl.pathname) && !isValidToken) {
-    return NextResponse.next();
-  }
-  if (isValidToken) {
-    return NextResponse.next();
-  }
-  return NextResponse.redirect(
-    new URL(
-      `/auth/login?from=${encodeURIComponent(request.nextUrl.pathname)}`,
-      request.nextUrl
-    ).href
-  );
+  // let isValidToken = false;
+
+  // const checkToken = await fetch(
+  //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/verify-token`,
+  //   {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${authToken}`,
+  //     },
+  //   }
+  // );
+  // if (checkToken.status === 200) {
+  //   isValidToken = true;
+  // }
+  // // if the request url contains /auth/login or /auth/signin and isValidToken is true then redirect to home page
+  // if (isLoginRoutes(request.nextUrl.pathname) && isValidToken) {
+  //   return NextResponse.redirect(new URL('/', request.nextUrl).href);
+  // }
+  // // if the request url contains /auth/login or /auth/signin and isValidToken is false then return the request
+  // if (isLoginRoutes(request.nextUrl.pathname) && !isValidToken) {
+  //   return NextResponse.next();
+  // }
+  // if (isValidToken) {
+  //   return NextResponse.next();
+  // }
+  // return NextResponse.redirect(
+  //   new URL(
+  //     `/auth/login?from=${encodeURIComponent(request.nextUrl.pathname)}`,
+  //     request.nextUrl
+  //   ).href
+  // );
 }
