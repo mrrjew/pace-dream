994843be61dcbcebbff6d9a8ba8b1002b3b31f82
developer-shajib <sunhailshajib1@gmail.com>
2024-01-04 23:16:47 +0600

    Changed logo, favicon, navigation and remove dark mode option

--- ChisfisNextjs/src/app/(client-components)/(Header)/MainNav1.tsx
+++ ChisfisNextjs/src/app/(client-components)/(Header)/MainNav1.tsx
@@ -6,7 +6,7 @@ import ButtonPrimary from '@/shared/ButtonPrimary';
 import MenuBar from '@/shared/MenuBar';
 import SwitchDarkMode from '@/shared/SwitchDarkMode';
 import HeroSearchForm2MobileFactory from '../(HeroSearchForm2Mobile)/HeroSearchForm2MobileFactory';
-import PaceDreamImage from '@/images/PaceDreamLogo .svg';
+import PaceDreamImage from '@/images/paceDreamLogo-light.png';
 
 export interface MainNav1Props {
   className?: string;
@@ -15,31 +15,36 @@ export interface MainNav1Props {
 const MainNav1: FC<MainNav1Props> = ({ className = '' }) => {
   return (
     <div className={`nc-MainNav1 relative z-10 ${className}`}>
-      <div className="px-4 lg:container h-20 relative flex justify-between">
-        <div className="hidden md:flex justify-start flex-1 space-x-4 sm:space-x-10">
-          <Logo className="w-24 self-center" img={PaceDreamImage} />
+      <div className='px-4 lg:container h-20 relative flex justify-between'>
+        <div className='hidden md:flex justify-start flex-1 space-x-4 sm:space-x-10'>
+          <Logo
+            className='w-32 self-center'
+            img={PaceDreamImage}
+          />
           <Navigation />
         </div>
 
-        <div className="flex lg:hidden flex-[3] max-w-lg !mx-auto md:px-3 ">
-          <div className="self-center flex-1">
+        <div className='flex lg:hidden flex-[3] max-w-lg !mx-auto md:px-3 '>
+          <div className='self-center flex-1'>
             <HeroSearchForm2MobileFactory />
           </div>
         </div>
 
-        <div className="hidden md:flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100">
-          <div className="hidden xl:flex space-x-0.5">
+        <div className='hidden md:flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100'>
+          <div className='hidden xl:flex space-x-0.5'>
             <SwitchDarkMode />
-            <SearchDropdown className="flex items-center" />
-            <div className="px-1" />
-            <ButtonPrimary className="self-center" href="/auth/login">
+            <SearchDropdown className='flex items-center' />
+            <div className='px-1' />
+            <ButtonPrimary
+              className='self-center'
+              href='/auth/login'>
               Sign up
             </ButtonPrimary>
           </div>
 
-          <div className="flex xl:hidden items-center">
+          <div className='flex xl:hidden items-center'>
             <SwitchDarkMode />
-            <div className="px-0.5" />
+            <div className='px-0.5' />
             <MenuBar />
           </div>
         </div>
