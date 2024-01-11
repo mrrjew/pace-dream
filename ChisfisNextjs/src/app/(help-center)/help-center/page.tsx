5fee1dd6a9df24c7eab2454ab3d210df28f27cd1
Gaurav <gauravhpanchal2@gmail.com>
2024-01-11 08:55:30 +0530

    Build error fixed

--- ChisfisNextjs/src/app/(help-center)/help-center/page.tsx
+++ ChisfisNextjs/src/app/(help-center)/help-center/page.tsx
@@ -3,18 +3,15 @@ import ReachUsPage from "@/components/HelpCenter/ReachUsPage";
 import SeeAllPage from "@/components/HelpCenter/SeeAllPage";
 import SupportPage from "@/components/HelpCenter/SupportPage";
 
-interface HelpCenterProps {
-    selected: string;
-  }
-
-  const HelpCenter: React.FC<HelpCenterProps> = ({ selected }) => {  return (
+const HelpCenter: React.FC<{ selected: string }> = ({ selected }) => {
+  return (
     <div className="pb-30">
-      {selected === 'Contact Us' && <ContactUsPage />}
-      {selected === 'Contacting us for support' && <SupportPage />}
-      {selected === 'Where you can reach us' && <ReachUsPage />}
-      {selected === 'See all' && <SeeAllPage />}
+      {selected === "Contact Us" && <ContactUsPage />}
+      {selected === "Contacting us for support" && <SupportPage />}
+      {selected === "Where you can reach us" && <ReachUsPage />}
+      {selected === "See all" && <SeeAllPage />}
     </div>
-  )
-}
+  );
+};
 
 export default HelpCenter;
