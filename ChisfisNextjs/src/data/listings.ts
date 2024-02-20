4f2b136a72612cd40cc8109e17469eb31cf6319d
Prateek Kumar <96932336+Prateek043@users.noreply.github.com>
2024-02-21 02:14:14 +0530

    Update listings.ts

--- ChisfisNextjs/src/data/listings.ts
+++ ChisfisNextjs/src/data/listings.ts
@@ -77,11 +77,16 @@ const DEMO_TIMEBASED_LISTINGS = __timeBasedListing.map((post, index):TimeBasedDa
   }
 })
 
-const DEMO_FLIGHT_LISTINGS = __flightListing.map((post, index):FlightDataType => {
-  return {
-    ...post
-  }
-})
+// const DEMO_FLIGHT_LISTINGS = __flightListing.map((post, index):FlightDataType => {
+//   return {
+//     ...post
+//   }
+// })
+const DEMO_FLIGHT_LISTINGS = __flightListing.map((post, index):any => {
+//   return {
+//     ...post
+//   }
+// })
 
 const DEMO_LAST_LISTINGS = __lastMinutesListing.map((post, index): LastDataType => {
   const category = DEMO_LAST_CATEGORIES.filter(
