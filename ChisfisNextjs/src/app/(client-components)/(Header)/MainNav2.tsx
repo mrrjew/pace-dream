74f2cad005776d72e635b7ef73e90f9146bafb15
Prateek Kumar <chaudharyprateek1999@gmail.com>
2024-02-22 22:14:00 +0530

    Logo Updated

--- ChisfisNextjs/src/app/(client-components)/(Header)/MainNav2.tsx
+++ ChisfisNextjs/src/app/(client-components)/(Header)/MainNav2.tsx
@@ -51,7 +51,7 @@ const MainNav2: FC<MainNav2Props> = ({ className = "" }) => {
     <header className={`MainNav2 relative z-10 ${className}`}>
       <section className="px-4 h-16 lg:container flex justify-between">
         <article className="hidden md:flex justify-between flex-1 space-x-3 sm:space-x-8 lg:space-x-10">
-          <Logo className="w-32 self-center" />
+          <Logo className="w-34  self-center" />
           {/* <div className='hidden lg:block self-center h-10 border-l border-neutral-300 dark:border-neutral-500'> */}
           <div className="hidden lg:flex  justify-end ">
             <button className={`${btnStyle}`} onClick={openModalCurrency}>
