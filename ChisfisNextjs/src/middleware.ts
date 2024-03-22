242d9057e73e2a52e3c3be8ece1664001f70681d
keremcankurt <keremkurt278@gmail.com>
2024-03-22 12:44:25 +0300

    update

--- ChisfisNextjs/src/middleware.ts
+++ ChisfisNextjs/src/middleware.ts
@@ -19,7 +19,7 @@ export async function middleware(request: NextRequest) {
   const cookies = request.cookies;
   const authToken = cookies.get('auth-token')?.value;
 
-  let isValidToken = true;
+  let isValidToken = false;
 
   const checkToken = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/verify-token`,
