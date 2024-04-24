da51bbe9c0bc82519917b0cf827485ad7a8ef60e
SAHIL sanghar <sangharsahil546@gmail.com>
2024-04-24 21:55:59 +0530

    Docs: Added HeroSearchForm Component

--- ChisfisNextjs/src/components/Hero.tsx
+++ ChisfisNextjs/src/components/Hero.tsx
@@ -8,6 +8,7 @@ import HeroImg1 from "@/images/Hero-Right-New-1.png"
 import HeroImg2 from "@/images/Hero-Right-New-2.png"
 import Vector from "@/images/Vector.png"
 import { HeroImage } from "@/assets";
+import HeroSearchForm from "@/app/(client-components)/(HeroSearchForm)/HeroSearchForm";
 
 const Hero = () => {
     return (
@@ -16,7 +17,7 @@ const Hero = () => {
                 {/* <Image src={arrowBg} alt="arrowBg" className="absolute object-contain h-20 w-60 md:top-28 top-20 right-2 md:-right-10 " /> */}
                 {/* <Image src={ellipse} alt="ellipse" className="absolute object-contain -left-40 -top-14 " /> */}
                 <p className="text-4xl font-semibold text-left leading-relaxed tracking-wider md:text-5xl lg:text-[40px] text-[#0C051D]">
-                    Discover Your Perfect Match: Book Rooms, Find Roommates, and Secure Last-Minute Deals Effortlessly!
+                    Perfect Match: Book Rooms, Find Roommates, Get Last Minute Deals with Ease!
                 </p>
                 <p className="block text-4xl font-semibold md:hidden ">
                     Discover Your Perfect <br />  Match: Book Rooms, <br /> Find  Roommates, <br /> and Secure <br />  Last-Minute Deals <span className="underline">Effortlessl</span>y!
@@ -27,11 +28,18 @@ const Hero = () => {
                 <p className="block mt-8 text-lg font-medium md:hidden font" >
                     An all-in-one platform that helps you to find what you needed.
                 </p>
-                <div className="flex gap-4 mt-8 ml-4 md:gap-4 md:ml-0">
+                <section
+        className={`nc-SectionHero flex flex-col-reverse md:flex-col md:pt-44 lg:pt-20 lg:pl-24 lg:pr-24 pt-4 md:max-w-[100%]`}
+        >
+        <div className="md:block w-max-[95vw] w-[93vw] xl:w-[80vw] lg:w-[90vw] flex md:pt-0 z-10 mb-12 ml-16 md:ml-0 lg:mb-0 md:mt-8 lg:-mt-16 md:w-10/12 border-grey border bg-white rounded-2xl">
+            <HeroSearchForm />
+        </div>
+        </section>
+                {/* <div className="flex gap-4 mt-8 ml-4 md:gap-4 md:ml-0">
                     <button className=" bg-[#5527D7] font-medium rounded-full md:px-8 md:py-4 px-4 py-4 text-white flex gap-4 items-center">Start your search <FaArrowRight /> </button>
                     <button className="flex rounded-full  md:px-8 md:py-4 px-4 py-4 gap-2 font-medium items-center text-[#5527D7] "><FiYoutube /> How it works</button>
-                </div>
-                <div className="grid grid-cols-3 gap-8 mt-10 ml-4 md:ml-2 w-96">
+                </div> */}
+                {/* <div className="grid grid-cols-3 gap-8 mt-10 ml-4 md:ml-2 w-96">
                     <div className="flex flex-col gap-2">
                         <p className="font-bold text-2xl text-center text-[#5527D7]">8,00,00+</p>
                         <hr className="w-full h-[3px] bg-gray-300 border-0 rounded dark:bg-gray-700" />
@@ -47,11 +55,12 @@ const Hero = () => {
                         <hr className="h-[3px] bg-gray-300 border-0 rounded dark:bg-gray-700" />
                         <p className="text-[15px] text-center font-medium">Experiences</p>
                     </div>
-                </div>
+                </div> */}
             </div>
-            <div className="lg:w-[520px] h-[500px] w-96 lg:mx-0 ml-[72px] relative">
-                <Image src={HeroImage} alt="Hero-right" fill className="object-contain z-1 " />
+            <div className="lg:w-[520px] h-[250px] w-96 lg:mx-0 ml-[72px] relative">
+                <Image src={HeroImage} alt="Hero-right" fill className="object-contain z-2 " />
             </div>
+            <br />
         </div>
     )
 }
