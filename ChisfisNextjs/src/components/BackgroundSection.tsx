9619ae9157da75b13e36c0c119d52e545feed41a
SAHIL sanghar <sangharsahil546@gmail.com>
2024-05-09 19:05:37 +0530

    Refactor: Reduced Height

--- ChisfisNextjs/src/components/BackgroundSection.tsx
+++ ChisfisNextjs/src/components/BackgroundSection.tsx
@@ -11,7 +11,7 @@ const BackgroundSection: FC<BackgroundSectionProps> = ({
 }) => {
   return (
     <div
-      className={`nc-BackgroundSection bg-violet rounded-2xl absolute inset-y-0 h-[700px] w-screen xl:max-w-[1340px] 2xl:max-w-screen-2xl left-1/2 transform -translate-x-1/2  ${className}`}
+      className={`nc-BackgroundSection bg-violet rounded-2xl absolute inset-y-0 h-[630px] w-screen xl:max-w-[1340px] 2xl:max-w-screen-2xl left-1/2 transform -translate-x-1/2  ${className}`}
       data-nc-id="BackgroundSection"
     >
       {children}
