23a62b370a944cf4ba6b5d4907a2d026279c1f0c
Gaurav <gauravhpanchal2@gmail.com>
2024-05-31 01:38:43 +0530

    Type error fixed from headers

--- ChisfisNextjs/src/app/(client-components)/(Header)/Header3.tsx
+++ ChisfisNextjs/src/app/(client-components)/(Header)/Header3.tsx
@@ -189,7 +189,7 @@ const Header3: FC<Header3Props> = ({ className = '' }) => {
               <div className=" flex space-x-1">
               {token ? (
             <Link
-              href={'/add-listing' as Route<String>}
+              href={'/add-listing'}
               className="self-center text-opacity-90 group px-4 py-2 border border-neutral-300 hover:border-neutral-400 dark:border-neutral-700 rounded-full inline-flex items-center text-sm text-gray-700 dark:text-neutral-300 font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
             >
               Post a Listing
