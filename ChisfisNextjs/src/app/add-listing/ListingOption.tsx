90162a2dec3b347ec1f7435afb3b6537e7dbe7c2
Gaurav <gauravhpanchal2@gmail.com>
2024-06-03 10:35:30 +0530

    ListingOption errors fixed

--- ChisfisNextjs/src/app/add-listing/ListingOption.tsx
+++ ChisfisNextjs/src/app/add-listing/ListingOption.tsx
@@ -13,7 +13,7 @@ const ListingOption = ({ href, img, alt, label }: ListingOptionProps) => {
   return (
     <Link
       className="p-4 hover:bg-[#632DF8] flex flex-col justify-center items-center sm:w-44 h-40 w-auto text-black border shadow-md border-[F6F4F6] hover:text-white text-[14px] font-[500] rounded-[25px] lg:rounded-[9px]"
-      href={href as Route<string>}
+      href={href as Route}
       type="button"
     >
       <div className="content-center mb-4">
