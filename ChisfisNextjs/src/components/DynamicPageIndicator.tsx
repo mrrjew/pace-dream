42a2b1790f719d54d8173de45c89102fb0d1c3f4
mrr.jew <100996700+mrrjew@users.noreply.github.com>
2024-06-04 07:39:41 +0000

    refactored settings pages

---
+++ ChisfisNextjs/src/components/DynamicPageIndicator.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { FaChevronRight } from "react-icons/fa6";
+
+const page = ({ main, curr_route }: any) => {
+    return (
+      <div>
+        <p className="flex items-center gap-2 text-sm">
+          <Link href="/acc-setting">{main}</Link>
+          <FaChevronRight />
+          {curr_route}
+        </p>
+      </div>
+    );
+  };
+  export default page;
